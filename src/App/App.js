1532bc50bdb20b55be5612af015970d8a73610ad
Zoe Ames <zoeames@gmail.com>
2020-07-01 19:27:22 -0500

    added sms

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.scss';
+import twilioData from '../helpers/data/twilioData';
 
 class App extends React.Component {
+  sendSMS = () => {
+    const numfromFirebase = '1234567890';
+    const smsNum = `+1${numfromFirebase}`;
+    const message = 'got a message from react';
+    twilioData.sendSMS(smsNum, message);
+  }
+
   render() {
     return (
       <div className="App">
         <h1>INSIDE APP COMPONENT</h1>
-        <button className="btn btn-info">I am a button</button>
+        <button className="btn btn-info" onClick={this.sendSMS}>I am a button</button>
       </div>
     );
   }
